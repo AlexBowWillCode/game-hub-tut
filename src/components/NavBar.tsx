b8f0dbe59b3fc0dea54a42ee5d4443af23dc6614
AlexBowWillCode <alexanderbowwill@gmail.com>
2023-12-08 13:08:28 +0000

    Filter games by genre

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,23 @@
-import { HStack, Image } from "@chakra-ui/react";
+import { HStack, Image, Text } from "@chakra-ui/react";
 import React from "react";
-import logo from "../assets/logo.webp";
+import logo from "../assets/logo2.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
     <HStack justifyContent="space-between" padding="10px">
-      <Image src={logo} boxSize="60px"></Image>
+      <Image src={logo} boxSize="60px" borderRadius={10}></Image>
+      <Text
+        fontSize="2xl"
+        backgroundColor="#B794F4"
+        borderRadius={10}
+        padding={3}
+      >
+        {" "}
+        JustIT Games Project
+      </Text>
       <ColorModeSwitch></ColorModeSwitch>
     </HStack>
   );
 };
-
 export default NavBar;
